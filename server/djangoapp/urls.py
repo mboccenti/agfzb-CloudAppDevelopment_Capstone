eb6e81bf204b0a39d225f906bbae8154fff23173
mboccenti <marco.boccenti@gmail.com>
2022-10-15 04:44:14 -0400

    mboccenti

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,26 +10,26 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='', view=views.about, name='about'),
-    
+    path(route='about/', view=views.about, name='about'),
+
     # path for contact us view
-    path(route='', view=views.contact, name='contact'),
+    path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
-    path(route='', view=views.registration_request, name='register'),
+    path('registration/', views.registration_request, name='registration'),
     
     # path for login
-    path(route='', view=views.login_request, name='login'),
-    
+    path('login/', views.login_request, name='login'),
+
     # path for logout
-    path(route='', view=views.logout_request, name='logout'),
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
-    path(route='dealer/<int:dealer_id>/', view=views.add_review, name='add_review'),
+    path('dealer/<int:dealer_id>/add_review', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
